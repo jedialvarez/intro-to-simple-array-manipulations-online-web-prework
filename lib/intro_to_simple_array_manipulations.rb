57a4e28d201b20bb850079ff72b178efafba6c67
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-16 18:30:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,8 +30,9 @@ def using_concat(my_favorite_things, more_favs)
 my_favorite_things.concat(more_favs)
 end
 
-def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-list_of_esoteric_programming_languages == another_esoteric_language.insert(4)
+def using_insert(array, element)
+array.insert(4)
+
 end
 
 def using_uniq(captain_planet_and_the_planeteers)
