d2c09d1659fddb86d290320a30892d78c8b22562
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-16 17:13:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,7 +26,7 @@ def shift_with_args(ice_cream_brands)
 ice_cream_brands.shift(2)
 end
 
-def using_concat(concat)
+def using_concat(my_favorite_things, more_favs)
 ["raindrops on roses", "whiskers on kittens"].concat( ["raindrops on roses", "whiskers on kittens"] )
 concat
 end
