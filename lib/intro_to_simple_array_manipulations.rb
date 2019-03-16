d6e1afbb4a6392e90e53b33e33a2cb92f1f298a9
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-16 17:42:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,9 +30,7 @@ def using_concat(my_favorite_things, more_favs)
 my_favorite_things.concat(more_favs)
 end
 
-def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-another_esoteric_language =  ["Malbolge"]
+def using_insert(new_array)
 new_array.insert(list_of_esoteric_programming_languages, another_esoteric_language)
 end
 
