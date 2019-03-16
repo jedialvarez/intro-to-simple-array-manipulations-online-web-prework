7cf7fa640b55251dad54bfa09e659017335bd8ef
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-16 18:10:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -40,7 +40,8 @@ def using_uniq(captain_planet_and_the_planeteers)
   captain_planet_and_the_planeteers.uniq
 end
 
-def using_flatten()
+def using_flatten(private_colleges_in_newyork)
+  private_colleges_in_newyork.flatten
 end
 
 def using_delete
